3780f2dbafafa37c6bcd9d71454bc42aa077a67b
Victor Gabriel <victorgabriel08fs@gmail.com>
2022-08-12 16:02:33 -0300

    first commit

---
+++ src/application/usecases/create-challenge-submission.ts
@@ -0,0 +1,14 @@
+import { Submission } from "../../domain/entities/submission";
+
+type CreateChallengeSubmissionRequest = {
+    studentId: string;
+  challengeId: string;
+};
+
+export class CreateChallengeSubmission {
+  async execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) {
+    const submission = Submission.create({ studentId, challengeId });
+
+    return submission;
+  }
+}
---
+++ src/domain/entities/challenge.ts
@@ -0,0 +1,17 @@
+import { Entity } from "../../core/domain/entity";
+
+type ChallengeProps = {
+  inscructionsUrl: string;
+  title: string;
+};
+
+export class Challenge extends Entity<ChallengeProps> {
+  private constructor(props: ChallengeProps, id?: string) {
+    super(props, id);
+  }
+
+  static create(props: ChallengeProps, id?: string) {
+    const challenge = new Challenge(props, id);
+    return challenge;
+  }
+}
