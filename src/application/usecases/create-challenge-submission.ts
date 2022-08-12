17e197b0f7d72795586a3d15f3fb5ecdfbf72e37
Victor Gabriel <victorgabriel08fs@gmail.com>
2022-08-12 17:43:59 -0300

    teste de existência de estudante e desafio implementado

--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,12 +1,28 @@
 import { Submission } from "../../domain/entities/submission";
+import { StudentsRepository } from "../repositories/StudentsRepository";
+import { ChallengesRepository } from "../repositories/ChallengesRepository";
 
 type CreateChallengeSubmissionRequest = {
-    studentId: string;
+  studentId: string;
   challengeId: string;
 };
 
 export class CreateChallengeSubmission {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private challengesRepository: ChallengesRepository
+  ) {}
+
   async execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) {
+    const student = await this.studentsRepository.findById(studentId);
+    if (!student) {
+      throw new Error("Student does not exists");
+    }
+    const challenge = await this.challengesRepository.findById(challengeId);
+    if (!student) {
+      throw new Error("Challenge does not exists");
+    }
+
     const submission = Submission.create({ studentId, challengeId });
 
     return submission;
